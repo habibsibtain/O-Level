d2d052b0a2ee4a7333854dbffafdbd21a7e10246
habibsibtain <sibtainhabib413@gmail.com>
2026-01-07 15:26:09 +0530

    first commit

---
+++ Backend/server.js
@@ -0,0 +1,11 @@
+import app from "./app.js";
+import http from "http"
+
+const port = process.env.PORT || 3000
+const server = http.createServer(app)
+
+server.listen(port, ()=>{
+  console.log("Server running on port: ", port)
+})
+
+
