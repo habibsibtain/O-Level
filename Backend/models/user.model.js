2b7d100e151963064f69bcab4473a4812c355b28
habibsibtain <sibtainhabib413@gmail.com>
2026-01-07 16:15:06 +0530

    user model

---
+++ Backend/models/user.model.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose"
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+
+const userSchema = new mongoose.Schema({
+  firstname: {
+    type: String,
+    required: true,
+    minLength: [3, "Firstname must be at least 3 characters long"]
+  },
+  lastname: {
+    type: String,
+    minLength: [3, "Lastname must be at least 3 characters long"]
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+\@.+\..+/, "Please fill a valid email address"]
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: [6, "Password must be at least 6 characters long"],
+    select: false
+  },
+  socketId: {
+    type: String,
+    default: null
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+usesrSchema.methods.generateAuthToken = function() {
+  const token = jwt.sign(
+    { _id: this._id},
+    process.env.JWT_SECRET,
+  );
+  return token;
+}
+
+userSchema.methods.comparePassword = async function(enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+}
+
+userSchema.statics.hashPassword = async function(password) {
+  const salt = await bcrypt.genSalt(10);
+  return await bcrypt.hash(password, salt);
+}
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
